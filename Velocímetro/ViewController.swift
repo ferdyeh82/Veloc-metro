58b66860c4c86539a7f0ef9840d1e81e2022c3a6
Fernando Espinosa Hernández <ferdyeh@gmail.com>
2016-09-05 18:34:48 +0100

    Initial Commit

---
+++ Velocímetro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velocímetro
+//
+//  Created by Fernando Espinosa Hernández on 5/9/16.
+//  Copyright © 2016 Fernando Espinosa Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VelocímetroTests/Veloci_metroTests.swift
@@ -0,0 +1,36 @@
+//
+//  Veloci_metroTests.swift
+//  VelocímetroTests
+//
+//  Created by Fernando Espinosa Hernández on 5/9/16.
+//  Copyright © 2016 Fernando Espinosa Hernández. All rights reserved.
+//
+
+import XCTest
+@testable import Veloci_metro
+
+class Veloci_metroTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
